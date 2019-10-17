166cd93c3fdcccfcf169488393e383486c41f698
Do Hoang <shinkenger.vn9x@gmail.com>
2019-10-17 14:05:18 +0700

    - Fix validator

--- src/main/kotlin/com/busticket/booking/request/OutJoinPolicyRequest.kt
+++ src/main/kotlin/com/busticket/booking/request/OutJoinPolicyRequest.kt
@@ -1,11 +1,9 @@
 package com.busticket.booking.request
 
-import javax.validation.constraints.NotEmpty
 import javax.validation.constraints.NotNull
 
 class OutJoinPolicyRequest(
         @field:NotNull
-        @field:NotEmpty
         var userId: Int? = null
 ) {
 }
